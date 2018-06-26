fd9eabb9ada7885b4f5a83ad8357e9d7264d03de
divyasundharam <divyasundharam8@gmail.com>
2018-06-26 08:32:33 +0000

    Create Chec.java

---
+++ Chec.java
@@ -0,0 +1,23 @@
+# Chec
+import java.io.*;
+import java.util.*;
+class Chec
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+{
+System.out.print("Positive);
+}
+else if(a<0)
+{
+System.out.print("Negative");
+}
+else
+{
+System.out.print("Zero");
+}
+}
+}
